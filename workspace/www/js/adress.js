5e413ed0408b4e993b859728ab3b5bbc49c9bdb9
Robert <robert.heck@griffithuni.edu.au>
2014-10-01 01:41:26 +0000

    1-10-14

--- workspace/www/js/adress.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $("address").each(function(){                         
-    var embed ="<iframe width='425' height='350' frameborder='0' scrolling='no'  marginheight='0' marginwidth='0'   src='https://www.google.com/maps/embed/v1/place?key=AIzaSyBHDbyc9TApTKqHTu-hjqb_CXY_6s8bqhQ
-            &q=putt+putt+in+mermaid+beach&zoom=16"+ encodeURIComponent( $(this).text() ) +"&amp;output=embed&iwloc'></iframe>";
-                                $(this).html(embed);
-                             
-   });
-});
